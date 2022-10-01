b4483db660407c340745ec96385b11db25cf14d5
Fahmidur-arnob <arnobdev93@gmail.com>
2022-10-01 11:38:55 +0600

    Added some buttons to the site and changed class to className

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is arnob.</h1>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+
+      <button type="button" className="btn btn-link">Link</button>
     </div>
   );
 }
